0c877c067ea348744f2aca818c5b89dcd042f822
Rachel Mullen <mullen.rachel@gmail.com>
2022-08-14 10:23:10 -0600

    commit tree d311c4bc76900f444dc071a92a90d2678b69c096

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+let employee = {
+    name: 'value1',
+    streetAddress: 'value 2'
+};
+
+function updateEmployeeWithKeyAndValue (obj, key, value) {
+    let NewEmployee = {...obj};
+}
